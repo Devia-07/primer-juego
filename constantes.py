cb5b8ad70e3898b7bd8ed95f9352faceef00f0fb
Devia-07 <devia.sebastian@univalle.edu.co>
2024-05-29 15:53:39 -0500

    agrege enemigo

--- constantes.py
+++ constantes.py
@@ -3,4 +3,5 @@ alto = 750
 color_bg = (0,0,20)
 fps = 60
 velocidad = 5
-escala_personaje = 1
+escala_personaje = 0.5
+escala_enemigo = 3
--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import pygame
 import constantes 
 from personajes import Personaje
+import os
+
+
+# funciones:
+
+# Funcion para escalar una imagen
+
+def invertir_imagen(imagen):
+    return pygame.transform.flip(imagen, True, False)
+
+def escalar_imagen(jugar_imagen, escala):
+    return pygame.transform.scale(jugar_imagen,
+                                        (jugar_imagen.get_width()*escala,
+                                         jugar_imagen.get_height()*escala))
+    
+    
+
+# funcion para contar elementos:
+def contar_elementos(directorio):
+    if os.path.exists(directorio):
+        return len(os.listdir(directorio))
+    
+
+
+# funcion listar nombres elementos:
+def nombres_carpetas(directorio):
+    return os.listdir(directorio)
+
 
 
 pygame.init()
@@ -12,21 +40,45 @@ ventana = pygame.display.set_mode((constantes.ancho, constantes.alto))
 # titulo de la ventana
 pygame.display.set_caption("Juego de Funciones")
 
-def escalar_imagen(jugar_imagen, escala):
-    return pygame.transform.scale(jugar_imagen,
-                                        (jugar_imagen.get_width()*escala,
-                                         jugar_imagen.get_height()*escala))
 animaciones = []
 for i in range(1,5):
     img = pygame.image.load(f"assets//imagenes//characters//player//{i} imagen.png")
     img = escalar_imagen(img, constantes.escala_personaje)
     animaciones.append(img)
 
+# enemigos:
+directorio_enemigos = "assets\imagenes\characters\enemigo"
+tipo_enemigos = nombres_carpetas(directorio_enemigos)
+
+animaciones_enemigos = []
+
+for eni in tipo_enemigos:
+    lista_temp = []
+    ruta_temp = f"assets//imagenes//characters//enemigo//{eni}"
+    num_animaciones = contar_elementos(ruta_temp)
+    
+    for i in range(1, num_animaciones):
+        img_enemigo = pygame.image.load(f"{ruta_temp}//dragon_{i}.png")
+        img_enemigo = escalar_imagen(img_enemigo, constantes.escala_enemigo)
+        img_enemigo = invertir_imagen(img_enemigo) # invertir la imagen
+        lista_temp.append(img_enemigo)
+    animaciones_enemigos.append(lista_temp)
+
 
 jugador_imagen = pygame.image.load("assets//imagenes//characters//player//1 imagen.png")
 jugador_imagen = escalar_imagen(jugador_imagen, constantes.escala_personaje)
 
+#craer un jugador de la clase personaje
 jugador = Personaje(100, 100, animaciones)
+
+#craer un enemigo de la clase personaje
+dragon = Personaje(900,300, animaciones_enemigos[0])
+
+
+#crear una lista de enemigos
+lista_enemigos = []
+lista_enemigos.append(dragon)
+
 # definir las varibles de movimiento del jugador
 
 mover_arriba = False
@@ -74,13 +126,16 @@ while run == True:
     
     # mover el jugador
     jugador.movimiento(delta_Y, delta_X)
-    
+    #actualizar el jugador
     jugador.actualizar()
-    
+    # actualizar el enemigo
+    dragon.actualizar()
     # dibuja el jugador en la ventana
     
     jugador.dibujar(ventana)  
     
+    #dibuja el enemigo en la ventana
+    dragon.dibujar(ventana)
     
     
     
